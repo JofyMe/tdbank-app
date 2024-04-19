e55aaa5962f8102964cb10014ba707bca73babc0
JofyMe <mr.tretyak15usa@gmail.com>
2024-04-19 12:00:00 -0400

    Fix Menu + 2 page

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
-console.log('Hello, world');
+function showActivity() {
+    var activityBlock = document.getElementById('activity');
+    var detailsBlock = document.getElementById('details');
+
+    activityBlock.classList.remove('hidden');
+    detailsBlock.classList.add('hidden');
+}
+
+function showDetails() {
+    var activityBlock = document.getElementById('activity');
+    var detailsBlock = document.getElementById('details');
+
+    activityBlock.classList.add('hidden');
+    detailsBlock.classList.remove('hidden');
+}
