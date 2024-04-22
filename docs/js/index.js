0be7b3061e33aef1036556dcdc0bcbba546f32fb
JofyMe <mr.tretyak15usa@gmail.com>
2024-04-22 16:47:58 -0400

    Version for Github pages

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,1 +1,26 @@
-console.log('Hello, world');
+function showActivity() {
+    var activityBlock = document.getElementById('activity');
+    var detailsBlock = document.getElementById('details');
+
+    activityBlock.classList.remove('hidden');
+    detailsBlock.classList.add('hidden');
+}
+
+function showDetails() {
+    var activityBlock = document.getElementById('activity');
+    var detailsBlock = document.getElementById('details');
+
+    activityBlock.classList.add('hidden');
+    detailsBlock.classList.remove('hidden');
+}
+
+const buttons = document.querySelectorAll('.mainButton');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    buttons.forEach(btn => {
+      btn.classList.remove('active');
+    });
+    this.classList.add('active');
+  });
+});
